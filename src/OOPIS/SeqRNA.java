227c5211a2c4c6df9c7601e02abeb5a008e25751
PjNeo <philipnewman9@icloud.com>
2023-05-13 22:00:27 +0200

    Started implementing the subclass mothods part3

--- src/OOPIS/SeqRNA.java
+++ src/OOPIS/SeqRNA.java
@@ -17,10 +17,16 @@ class SeqRNA extends FastaRepresentation implements Calculateable {
      */
     @Override
     public double meltingPointCal() {
+        double var;
         if(this.getLength()< 14){
-            var = (this.getaCount()+this.gettCount())*2
+            var =
+                    (this.getaCount()+this.gettCount())*2+
+                    (this.getgCount()+this.getcCount())*4;
+        }else{
+            var =  64.9+((41 * (this.getgCount() + this.getcCount() - 16.4))
+                            /(this.getaCount() + this.gettCount()+this.getgCount()+this.getcCount()));
         }
-        return 0.0;
+        return var;
     }
     /**
      * Methode zur Molekulargewichtsbestimmung
